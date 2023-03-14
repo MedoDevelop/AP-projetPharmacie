2450e713503e1bf70d9e0658afbe42c692dc7c1a
Medhy Rano <rano.medhy@gmail.com>
2023-03-14 16:33:56 -0400

    script sql debug !

---
+++ API/bdpath.php
@@ -0,0 +1,3 @@
+<?php
+    include_once '../Modele/bd.inc.php';
+?>
--- Modele/medicament.php
+++ Modele/medicament.php
@@ -1,1 +1,19 @@
+<?php
+    include_once "bd.inc.php";
 
+    function getAllMedicament(){
+        $db = connexionPDO();
+        $req = "SELECT m.idMedoc,m.libelle,m.stock,f.libelle as forme
+                FROM medicament m
+                JOIN posologie p ON m.idPoso=p.idPoso
+                JOIN forme f ON p.idPoso=f.idForme
+                ";
+        $query = $db->query($req);
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function addMedicament(){
+        $db = connexionPDO();
+        $req = "SEL"
+    }
+?>

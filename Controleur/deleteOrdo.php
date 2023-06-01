51c9af9272d2c50861ff2a713061503238183bed
Medhy Rano <rano.medhy@gmail.com>
2023-06-01 07:40:07 -0400

    debug ordonnance

--- Controleur/deleteOrdo.php
+++ Controleur/deleteOrdo.php
@@ -1,10 +1,32 @@
 <?php 
-
+/*
     if(isset($_GET["ordonnance"]) && !empty($_GET["ordonnance"])){
         $idOrdo = $_GET["ordonnance"];
-        sendDelete("http://api.test/ordonnance/delete/$idOrdo");
-        header("Location: ?action=consultOrdo");
+        include_once 'Vue/deleteOrdo.php';
+        if(verifVar($_POST['supression'])){
+            sendDelete("http://api.test/ordonnance/delete/$idOrdo");
+        }else{
+            header("Location: ?action=consultOrdo");
+        }
+        
     }else{
         header("Location: index.php");
+    }
+*/
+
+    if(isset($_GET["ordonnance"])){
+	    
+        $idOrdo=$_GET["ordonnance"];
+    }else{
+        echo('<p align="center">Erreur aucune mutuelle sélectionnée</p>');
+    }
+    include_once("./Vue/header.html");	
+    include_once("./Vue/deleteOrdo.php");
+
+    if(isset($_POST['supprimer'])){
+        sendDelete("http://api.test/ordonnance/delete/$idOrdo");
+        header("Location: ?action=consultOrdo");
+    }elseif(isset($_POST["annuler"])){
+        header("Location: ?action=consultOrdo");
     }
 ?>
---
+++ Vue/deleteOrdo.php
@@ -0,0 +1,28 @@
+<body>
+<div class="columns is-mobile">
+  <div class="column is-three-fifths is-offset-one-fifth">
+  	
+
+    <!--Modal pour confirmation de suppression-->
+    <div class="modal is-active" id="leModal">
+      <div class="modal-background"></div>
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">Suppression de la mutuelle</p>
+           <a href="?action=consultOrdo"><button class="delete" aria-label="close"></button></a>
+          </header>
+          <section class="modal-card-body">
+            Êtes-vous sûr de vouloir supprimer cette ordonnance ?
+          </section>
+          <footer class="modal-card-foot">
+            <form action="?action=deleteOrdo&ordonnance=<?= $idOrdo ?>" method="POST" name="supression" id="suppression">
+              <input class="button is-danger" type="submit" name="supprimer" value="Supprimer" />
+                &nbsp;&nbsp;&nbsp;&nbsp;<input class="button is-primary" type="submit" name="annuler" value="Annuler"/>
+          </form>
+           
+          </footer>
+      </div>
+</div>
+</div>
+</div>
+</body>
